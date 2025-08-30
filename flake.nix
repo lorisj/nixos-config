55a0a6c4358f662aaf76c745c7f14fc79a38c703
Loris Jautakas <lorisjautakas@gmail.com>
2025-08-30 16:00:39 -0700

    fix inputs and config name

--- flake.nix
+++ flake.nix
@@ -16,18 +16,20 @@
       home-manager,
     }@inputs:
     let
-      overlays = [];
+      overlays = [ ];
       mkSystem = import ./lib/mksystem.nix {
-        inherit overlays nixpkgs home-manager;
+        inherit inputs overlays nixpkgs home-manager;
       };
       registry = import ./lib/registry.nix;
     in
     # make configuration for each entry in registry.configs
     # this returns attr set w/ nixosConfigurations.${machine} = mkSystem {inherit machine user OS} for each {machine user OS} in registry.configs
-    builtins.listToAttrs (
-      builtins.map (entry: {
-        name = "nixosConfigurations." + entry.machine;
-        value = mkSystem entry;
-      }) registry.configs
-    );
+    {
+      nixosConfigurations = builtins.listToAttrs (
+        builtins.map (entry: {
+          name = entry.machine;
+          value = mkSystem entry;
+        }) registry.configs
+      );
+    };
 }
