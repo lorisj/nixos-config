23f5decccec622f0db85c993a9ce9de28037d054
Loris Jautakas <lorisjautakas@gmail.com>
2025-08-31 20:43:37 -0700

    changed neovim to use system theme

--- modules/development/neovim/config.nix
+++ modules/development/neovim/config.nix
@@ -1,5 +1,7 @@
 { config, pkgs, nvf, inputs, ... }:
-{
+let 
+colors = config.colorScheme.palette;
+in{
     imports = [
 	nvf.homeManagerModules.default
     ];
@@ -8,16 +10,19 @@
         settings = {
 	    vim = {
                 package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
-		#theme = {
-		#    enable = true;
-	        #    name = "solarized";
-	        #    style = "dark";
-		#};
-		#languages = {
-		    #nix = {
-		    #	enable = true;
-		    #};
-		#};
+		theme = {
+		    enable = true;
+	            name = "base16";
+	            #style = "";
+              base16-colors = with colors; {
+                inherit base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F;
+              };
+		};
+		languages = {
+		    nix = {
+		    	enable = true;
+		    };
+		};
 	    };
         };
     };
