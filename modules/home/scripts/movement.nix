04338e876b791d69f1f91d15395ed8500da7db64
lorisj <lorisjautakas@gmail.com>
2025-10-23 14:19:31 -0700

    more binds

--- modules/home/scripts/movement.nix
+++ modules/home/scripts/movement.nix
@@ -1,6 +1,8 @@
 {
   programs.bash.shellAliases = {
     cdrr = "cd $(git rev-parse --show-toplevel)"; # goto repo root
+    vimrr = "vim $(git rev-parse --show-toplevel)";
     files = "thunar";
+    sd0 = "shutdown 0";
   };
 }
